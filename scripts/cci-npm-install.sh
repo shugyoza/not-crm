90f218101a4f38bef0dd7e543db01212e63a9260
Stephen Hanjaya <85083229+shugyoza@users.noreply.github.com>
2023-11-06 11:40:59 -0800

    20231105-1 - Add CircleCI and Docker Implementation (#2)
    * update package.json npm start script
    
    * add Dockerfile
    
    * add spindocker.sh
    
    * add .circleci/config.yml
    
    * update .cci/config.yml
    
    * update .cci/config.yml
    
    * update .cci/config.yml
    
    * update .cci/config.yml
    
    * update .cci/config.yml
    
    * try update Dockerfile
    
    * try revert Dockerfile,update config.yml
    
    * update .cci/config.yml
    
    * add scripts,update package.jsonand config.yml
    
    * update .cci/config.yml
    
    * update .cci/config.yml
    
    * update .cci/config.yml
    
    * update .cci/config.yml
    
    * update cci-npm-install.sh
    
    * update cci-npm-test.sh
    
    * update .cci/config.yml
    
    * update .cci/config.yml
    
    * update .cci/config.yml
    
    * update .cci/config.yml
    
    * update cci-npm-install.sh
    
    * update .cci/config.yml
    
    * update cci-npm-install.sh
    
    * update cci-npm-install.sh
    
    * update cci-npm-install.sh
    
    * update .cci/config.yml
    
    * update .cci/config.yml
    
    * update .cci/config.yml
    
    * update cci-npm-install.sh
    
    * update cci-npm-test.sh
    
    * update .cci/config.yml
    
    * update .cci/config.yml
    
    * update .cci/config.yml

---
+++ scripts/cci-npm-install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -eo pipefail
+#!/bin/bash
+echo "cci-npm-install.sh script is starting..."
+
+CURRENT_DIR="${PWD}"
+SUBPROJECT_DIR="project/not-crm"
+
+# install dependencies in the component repository first
+if [[ -f "$SUBPROJECT_DIR/package.json" ]]; then
+  cd $SUBPROJECT_DIR
+  echo "Installing dependencies in $PWD"
+  npm install --no-audit
+else
+  echo "No package was found in $SUBPROJECT_DIR/package.json"
+fi
+
+# Install root level dependencies
+cd $CURRENT_DIR
+echo "Installing dependencies in $PWD ..."
+
+echo "Listing everyting inside this current directory is: $CURRENT_DIR: $(ls -lah)"
+
+npm install --no-audit
