23c7dd885e2c85623e2c8b86939cf015a4fceb49
Stephen Hanjaya <stephenhanjaya@gmail.com>
2023-11-06 14:56:52 -0800

    update register.component.spec:add spec

--- src/app/features/register/register.component.spec.ts
+++ src/app/features/register/register.component.spec.ts
@@ -21,5 +21,36 @@ describe('RegisterComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should render', () => {});
+  it('should have the email updated when .onChange() got called with an object that has id = "email"', () => {
+    const inputEvent = {
+      id: 'email',
+      value: 'test@email.com',
+    };
+    component.onChange(inputEvent);
+    fixture.detectChanges();
+
+    expect(component.email).toEqual(inputEvent.value);
+  });
+
+  it('should have the password updated when .onChange() got called with an object that has id = "password"', () => {
+    const inputEvent = {
+      id: 'password',
+      value: 'test@password',
+    };
+    component.onChange(inputEvent);
+    fixture.detectChanges();
+
+    expect(component.email).toEqual(inputEvent.value);
+  });
+
+  it('should have the confirmPassword updated when .onChange() got called with an object that has id = "confirmPassword"', () => {
+    const inputEvent = {
+      id: 'confirmPassword',
+      value: 'test@password',
+    };
+    component.onChange(inputEvent);
+    fixture.detectChanges();
+
+    expect(component.email).toEqual(inputEvent.value);
+  });
 });
