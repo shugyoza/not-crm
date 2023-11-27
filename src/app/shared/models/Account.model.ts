04635e075e5528f7030d2f7a6dee3c31d1f0bb15
Stephen Hanjaya <stephenhanjaya@gmail.com>
2023-11-26 21:11:55 -0800

    rename and update lint issue

--- src/app/shared/models/Account.model.ts
+++ src/app/shared/models/Account.model.ts
@@ -1,7 +1,7 @@
-export interface User {
+export interface Account {
   id: string;
   username: string;
   password: string;
   role: 'owner' | 'admin' | 'user';
-  token: string
+  token: string;
 }
