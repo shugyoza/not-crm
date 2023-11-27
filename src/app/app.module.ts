c98ea96e45133876388ea5ef3b15ff7288ee10b6
Stephen Hanjaya <stephenhanjaya@gmail.com>
2023-11-26 18:59:41 -0800

    dangle comma and semicolons post updating .prettierrc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
-import { NgModule } from '@angular/core'
-import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module'
-import { SharedModule } from './shared/shared.module'
-import { RegisterModule } from './features/register/register.module'
+import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+import { RegisterModule } from './features/register/register.module';
 
-import { AppComponent } from './app.component'
+import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { ErrorsMessagePipe } from './shared/pipes/errors-message.pipe';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule, RegisterModule],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    SharedModule,
+    RegisterModule,
+  ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
