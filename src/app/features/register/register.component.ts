3fb2236a530f183f310d2de4ff204752a8a0911f
Stephen Hanjaya <stephenhanjaya@gmail.com>
2023-12-01 09:54:09 -0800

    update register.component for error handling + unsubscribe

--- src/app/features/register/register.component.ts
+++ src/app/features/register/register.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnDestroy } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { take } from 'rxjs';
+import { Subscription, take } from 'rxjs';
 
 import { length, valid, errorMessages } from '../../shared/constants';
 import { RegisterHttpService } from './register-http.service';
@@ -13,43 +13,39 @@ const { required, minLength, maxLength, pattern } = Validators;
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnDestroy {
   public errorMessages = errorMessages;
-  public loginFail = '';
+  loginFail = '';
+  private subscription$: Subscription | null = null;
 
   constructor(
-    private http: RegisterHttpService,
+    private registerHttpService: RegisterHttpService,
     private router: Router
   ) {}
 
-  public register = new FormGroup({
-    email: new FormControl('', [
-      required,
-      minLength(length.email.min),
-      maxLength(length.email.max),
-      pattern(valid.email),
-    ]),
-    username: new FormControl('', [
-      required,
-      minLength(length.username.min),
-      maxLength(length.username.max),
-      pattern(valid.username),
-    ]),
-    role: new FormControl({ value: 'user', disabled: true }),
-    password: new FormControl('', [
-      required,
-      minLength(length.password.min),
-      pattern(valid.password),
-    ]),
-    confirmPassword: new FormControl('', [required]),
-  });
+  public email = new FormControl<string | null>('', [
+    required,
+    minLength(length.email.min),
+    maxLength(length.email.max),
+    pattern(valid.email),
+  ]);
+  public password = new FormControl<string | null>('', [
+    required,
+    minLength(length.password.min),
+    pattern(valid.password),
+  ]);
+  public confirmPassword = new FormControl<string | null>('', [required]);
+
+  ngOnDestroy(): void {
+    this.subscription$?.unsubscribe();
+  }
 
   public onSubmit(): void {
-    const { username, email, password } = this.register.value;
+    const email = this.email.value || '';
+    const password = this.password.value || '';
 
-    if (!username || !email || !password) {
-      return;
-    }
+    const regex = /[@\.]/gi;
+    const username = email.replace(regex, '-');
 
     const form = {
       email,
@@ -58,30 +54,21 @@ export class RegisterComponent {
       role: 'user',
     };
 
-    this.http
+    this.registerHttpService
       .register(form)
       .pipe(take(1))
       .subscribe({
         next: () => {
           this.router.navigate(['/login']);
         },
-        error: (error: Error) => console.log(error),
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        error: (error: any) => {
+          if (error.message.indexOf('409 Conflict')) {
+            this.loginFail = 'email exists';
+          } else {
+            this.loginFail = 'something is wrong';
+          }
+        },
       });
-  }
-
-  get email() {
-    return this.register.get('email');
-  }
-
-  get username() {
-    return this.register.get('username');
-  }
-
-  get password() {
-    return this.register.get('password');
-  }
-
-  get confirmPassword() {
-    return this.register.get('confirmPassword');
   }
 }
