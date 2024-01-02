3f6c933d240038bb4c18da133333e850c637da26
Stephen Hanjaya <stephenhanjaya@gmail.com>
2024-01-01 20:30:58 -0800

    add app.config n app.routes

---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () =>
+    import('./features/login/login.component').then((m) => m.LoginComponent),
+  }
+];
